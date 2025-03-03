e94f2667782a7411c162a74959af7d03842a3b69
karthikkumarreddy51 <119151659+karthikkumarreddy51@users.noreply.github.com>
2025-03-03 09:54:04 +0530

    profiles

---
+++ src/components/DeleteProfileModal.jsx
@@ -0,0 +1,12 @@
+// components/DeleteProfileModal.jsx
+import React from 'react';
+
+const DeleteProfileModal = () => {
+  return (
+    <div className="reconciliation-container container-fluid p-3">
+      <h1>DeleteProfileModal</h1>
+    </div>
+  );
+};
+
+export default DeleteProfileModal ;
---
+++ src/components/EditProfileModal.jsx
@@ -0,0 +1,12 @@
+// components/EditProfileModal.jsx
+import React from 'react';
+
+const EditProfileModal = () => {
+  return (
+    <div className="reconciliation-container container-fluid p-3">
+      <h1>EditProfileModal</h1>
+    </div>
+  );
+};
+
+export default EditProfileModal ;
